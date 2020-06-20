4409911cbf7b963730fa8974829bf5899b8e8fa2
lzy305 <38307479+lzy305@users.noreply.github.com>
2020-06-20 12:22:15 +0800

    Update 用户标签具体应用场景.sql

--- 实验开发/用户标签具体应用场景.sql
+++ 实验开发/用户标签具体应用场景.sql
@@ -1,67 +1,36 @@
-
-  --3.1 测算人群 人数
-    select bitmapAndCardinality(user0, user1)
-      from
-        (select 1 as jion_id,
-               groupBitmapMergeState(users) as user0
-          from ch_tag_user
-          where label_id = 'sex'
-              ) tmp1
-    inner join
-       (select 1 as jion_id,
-               groupBitmapMergeState(users) as user1
-          from ch_tag_user
-          where label_id = 'pay_qty'
-              ) tmp2 on tmp1.jion_id = tmp2.jion_id;
-
-  --3.2 特定人群 对应相关标签
-    select label_id,label_value, bitmapAndCardinality(main_userlist, detail_userlist) as user_number
-      from
-          (select  1 as join_id, label_id, label_value, groupBitmapMergeState(users) as main_userlist
-              from  ch_tag_user group by label_id, label_value) main
-    inner join
-        (select jion_id, bitmapAnd( user0, user1) as detail_userlist
-          from
-            (select 1 as jion_id,
-                   groupBitmapMergeState(users) as user0
-              from ch_tag_user
-              where label_id = 'sex'
-                  ) tmp1
-        inner join
-           (select 1 as jion_id,
-                   groupBitmapMergeState(users) as user1
-              from ch_tag_user
-              where label_id = 'pay_qty'
-                  ) tmp2 on tmp1.jion_id = tmp2.jion_id
-              ) detail
-    on main.join_id = detail.jion_id;
-
-  --3.3 查看特定用户的全标签
-    select *
-        from
-    (select  label_id, label_value,groupBitmapMergeState(users) as main_userlist
-       from  ch_tag_user group by label_id, label_value
-        ) xx
-    where bitmapAndCardinality(bitmapBuild([toUInt64(900)]), main_userlist) >=1;
-
-  --3.4 用户清单ID
- select  arrayJoin(bitmapToArray(bitmapAnd( user0, user1))) as users
-          from
-            (select 1 as jion_id,
-                   groupBitmapMergeState(users) as user0
-              from ch_tag_user
-              where label_id = 'sex'
-                  ) tmp1
-        inner join
-           (select 1 as jion_id,
-                   groupBitmapMergeState(users) as user1
-              from ch_tag_user
-              where label_id = 'pay_qty'
-                  ) tmp2 on tmp1.jion_id = tmp2.jion_id
+---活动预热 选取特定用户群
+  --.性别为'女'  且 ( 最近60天有登录 或者 渠道 好友推荐过来)
+WITH
+   (select groupBitmapMergeState(ctud.users)
+         from tag_dt.ch_tag_user_int ctud
+        where ctud.label_id = 'A2011'
+        and ctud.label_value > 1000
+        ) AS user0,
+    (select groupBitmapMergeState(ctud.users)
+         from tag_dt.ch_tag_user_str ctud
+        where ctud.label_id = 'A1005'
+        and ctud.label_value = 'FRIENDS'
+        ) AS user1
+select bitmapOrCardinality(user0, user1)
 ;
 
 
----------------------------------------------------------------- 结论 ----------------------------------------------------------------
-1. 标签可以并行导入，加快用户画像就绪工作
 
-2. 可以做到实时增加标签
+
+
+ --- 活动期间效果分析
+select bitmapAnd()
+  from
+ (select 1 as join_id, groupBitmapMergeState(ctud.users) as user_0
+   from tag_dt.ch_tag_user_date ctud
+where ctud.label_id = 'A1009'
+  and ctud.label_value >= toDate('2019-06-01')
+  and ctud.label_value <  toDate('2019-07-01')
+     ) tmp1
+inner join
+  (select 1 as join_id, groupBitmapMergeState(ctud.users) as user_1
+   from tag_dt.ch_tag_user_date ctud
+where ctud.label_id = 'A1005'
+  and ctud.label_value >= toDate('2019-06-01')
+  and ctud.label_value <  toDate('2019-07-01')
+     ) tmp2 on tmp1.join_id = tmp2.join_id
