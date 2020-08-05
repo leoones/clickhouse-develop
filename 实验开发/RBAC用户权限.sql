f572fd01afa59a14a01c59e8600ea4f8ec2f3d29
lzy305 <38307479+lzy305@users.noreply.github.com>
2020-08-05 17:27:11 +0800

    Update RBAC用户权限.sql

--- 实验开发/RBAC用户权限.sql
+++ 实验开发/RBAC用户权限.sql
@@ -1,5 +1,6 @@
 开启rbac两步骤
   1, config.xml 中添加 access_control_path 配置项
-  
+    <access_control_path>/home/data/clickhouse/access/</access_control_path>
   2, user.xml 中为默认用户 default 添加 access_management 配置项
+     <access_management>1</access_management>
   
