5ae7bb7b8edb641cdeb74f4e44bece799af34a35
lzy305 <38307479+lzy305@users.noreply.github.com>
2020-08-18 19:52:22 +0800

    Update 编译打包.sh

--- 运维监控/编译打包.sh
+++ 运维监控/编译打包.sh
@@ -1,4 +1,13 @@
 --------------------------------------------------------- 前端 -----------------------------------------------------------------------
+--1.1 设置下载全局包地址
+npm root -g 
+npm config set prefix "C:\nodejs\node_global"
+
+npm config set prefix "C:\nodejs\node_cache"
+
+--设置源
+npm config set registry https://registry.npm.taobao.org
+
  --nrm是npm registry 管理工具，可以切换你想要的源
   sudo npm install nrm
 
