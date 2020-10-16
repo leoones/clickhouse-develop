5f9c73faa697780bf1c2c1681bdb64ee3769270d
lzy305 <38307479+lzy305@users.noreply.github.com>
2020-10-16 12:02:13 +0800

    Update RBAC.sql

--- 实验开发/SQL开发/RBAC.sql
+++ 实验开发/SQL开发/RBAC.sql
@@ -1,1 +1,5 @@
-12
+开启RBAC两步骤
+  1, config.xml 中添加 access_control_path 配置项
+    <access_control_path>/home/data/clickhouse/access/</access_control_path>
+  2, user.xml 中为默认用户 default 添加 access_management 配置项
+     <access_management>1</access_management
